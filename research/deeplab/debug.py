ff987473d6645339a714bc9f14b04f5ba7f9b956
Dennis Tan <dennis@formatech.com>
2019-09-16 16:27:29 -0700

    add data augmentation visualizeation to debug.py

--- research/deeplab/debug.py
+++ research/deeplab/debug.py
@@ -14,3 +14,27 @@ def plot_images(dataset, n_images, samples_per_image):
     plt.figure()
     plt.imshow(output)
     plt.show()
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.ion()
+
+iterator = dataset.get_one_shot_iterator()
+next_element = iterator.get_next()
+
+fig, axs = plt.subplots(1, 3)
+
+with tf.Session() as sess:
+    while True:
+        sample = sess.run(next_element)
+        original_image = sample['original_image'].astype(np.uint8).squeeze()
+        image = sample['image'].astype(np.uint8).squeeze()
+        label = sample['label'].squeeze()
+        label[0, 0] = 10       # consistent cmap label colors
+        label[label == 255] = 10
+
+        axs[0].imshow(original_image)
+        axs[1].imshow(image)
+        axs[2].imshow(label, cmap='gray')
+
+        import pdb; pdb.set_trace()
