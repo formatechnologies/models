f6490aa3ea73c2d3bf4912ef09a1a862b4b20219
Xin Yu Tan <dennis@formatech.com>
2019-09-17 16:06:24 -0700

    clean up local_test_forma.sh

--- research/deeplab/local_test_forma.sh
+++ research/deeplab/local_test_forma.sh
@@ -26,21 +26,15 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Move one-level up to tensorflow/models/research directory.
-cd ..
+# Set up the working environment.
+WORK_DIR=$(pwd)
 
 # Update PYTHONPATH.
+cd ..
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+cd ${WORK_DIR}
 
-# Fix cuDNN bug with RTX GPUS
-export TF_FORCE_GPU_ALLOW_GROWTH=true
-
-# Set up the working environment.
-CURRENT_DIR=$(pwd)
-WORK_DIR="${CURRENT_DIR}/deeplab"
-DATASET_DIR="datasets"
-
-# ========================== SETTINGS (START) ==========================
+# ========================== SETTINGS (DATASET) ==========================
 # http://hellodfan.com/2018/07/06/DeepLabv3-with-own-dataset/
 
 # DATASET_NAME="forma_1k"
@@ -48,87 +42,78 @@ DATASET_DIR="datasets"
 # DATASET_SEG_ENCODING_TYPE='seg_name_to_label'
 # NUM_CLASSES=9
 # EVAL_CROP_SIZE="1601,3313"
+# NUM_ITERATIONS=30000
+# SAVE_INTERVAL_SECS=120
+# SAVE_SUMMARIES_SECS=120
 
 # DATASET_NAME="forma_1k_3"
 # DATASET_SIZE=1000
 # DATASET_SEG_ENCODING_TYPE='seg_name_to_label_3'
 # NUM_CLASSES=3
 # EVAL_CROP_SIZE="1601,3313"
+# NUM_ITERATIONS=30000
+# SAVE_INTERVAL_SECS=120
+# SAVE_SUMMARIES_SECS=120
 
 # DATASET_NAME="forma_1k_7"
 # DATASET_SIZE=1000
 # DATASET_SEG_ENCODING_TYPE='seg_name_to_label_7'
 # NUM_CLASSES=7
 # EVAL_CROP_SIZE="1601,3313"
+# NUM_ITERATIONS=30000
+# SAVE_INTERVAL_SECS=120
+# SAVE_SUMMARIES_SECS=120
 
 DATASET_NAME="forma_37k"
 DATASET_SIZE=-1
 DATASET_SEG_ENCODING_TYPE='seg_name_to_label'
 NUM_CLASSES=9
 EVAL_CROP_SIZE="1601,3783"
+NUM_ITERATIONS=740000
+SAVE_INTERVAL_SECS=1200
+SAVE_SUMMARIES_SECS=600
 
 # DATASET_NAME="forma_37k_3"
 # DATASET_SIZE=-1
 # DATASET_SEG_ENCODING_TYPE='seg_name_to_label_3'
 # NUM_CLASSES=3
 # EVAL_CROP_SIZE="1601,3783"
+# NUM_ITERATIONS=740000
+# SAVE_INTERVAL_SECS=1200
+# SAVE_SUMMARIES_SECS=600
 
 # DATASET_NAME="forma_37k_7"
 # DATASET_SIZE=-1
 # DATASET_SEG_ENCODING_TYPE='seg_name_to_label_7'
 # NUM_CLASSES=7
 # EVAL_CROP_SIZE="1601,3783"
+# NUM_ITERATIONS=740000
+# SAVE_INTERVAL_SECS=1200
+# SAVE_SUMMARIES_SECS=600
 
-# DATASET_FOLDER="${WORK_DIR}/${DATASET_DIR}"
-DATASET_FOLDER="${HOME}/storage/shared/datasets/deeplab_experiments"
-FORMA_DATASET_DIR="${DATASET_FOLDER}/${DATASET_NAME}"
-mkdir -p "${FORMA_DATASET_DIR}"
+# ========================== SETTINGS (WORKSTATION) ==========================
 
-# NUM_ITERATIONS=30000
-# SAVE_INTERVAL_SECS=120
-# SAVE_SUMMARIES_SECS=120
-
-NUM_ITERATIONS=740000
-SAVE_INTERVAL_SECS=1200
-SAVE_SUMMARIES_SECS=600
-
+# Dennis Workstation Settings
+export TF_FORCE_GPU_ALLOW_GROWTH=true   # Workaround cuDNN bug with RTX GPUS
 TRAIN_BATCH_SIZE=1
 FINE_TUNE_BATCH_NORM=false
 
+# # GPU 1 + GPU 2 Workstation Settings
 # TRAIN_BATCH_SIZE=12
 # FINE_TUNE_BATCH_NORM=true
 
-TF_INITIAL_CHECKPOINT="${DATASET_FOLDER}/forma_37k_ckpt/model.ckpt-740000"
-INITIALIZE_LAST_LAYERS=true
-LAST_LAYERS_CONTAINS_LOGITS_ONLY=true # irrelevant
+# ========================== SETTINGS (PATHS) ==========================
 
-# TF_INITIAL_CHECKPOINT="${WORK_DIR}/${DATASET_DIR}/pascal_voc_seg/init_models/deeplabv3_pascal_train_aug/model.ckpt"
-# INITIALIZE_LAST_LAYERS=false
-# LAST_LAYERS_CONTAINS_LOGITS_ONLY=true
-
-# ========================== SETTINGS (END) ==========================
-
-# # Run model_test first to make sure the PYTHONPATH is correctly set.
-# python3 "${WORK_DIR}"/model_test.py -v
-
-# Go to datasets folder and download PASCAL VOC 2012 segmentation dataset.
-cd "${WORK_DIR}/${DATASET_DIR}"
-# sh download_and_convert_voc2012.sh
-
+# Dataset Paths
+DATASET_FOLDER="${HOME}/storage/shared/datasets/deeplab_experiments"
+FORMA_DATASET_DIR="${DATASET_FOLDER}/${DATASET_NAME}"
 FORMA_DATASET_DATA="${FORMA_DATASET_DIR}/tfrecord"
 FORMA_DATASET_LIST="${FORMA_DATASET_DIR}/dataset_split"
+mkdir -p "${FORMA_DATASET_DIR}"
 mkdir -p "${FORMA_DATASET_DATA}"
 mkdir -p "${FORMA_DATASET_LIST}"
-python3 ./build_forma_data.py \
-    --dataset_size=${DATASET_SIZE} \
-    --dataset_seg_encoding_type="${DATASET_SEG_ENCODING_TYPE}" \
-    --output_dir="${FORMA_DATASET_DATA}" \
-    --list_folder="${FORMA_DATASET_LIST}"
 
-# Go back to original directory.
-cd "${CURRENT_DIR}"
-
-# Set up the working directories.
+# Experiment Paths
 EXP_FOLDER="exp/train_on_train_set"
 INIT_FOLDER="${FORMA_DATASET_DIR}/init_models"
 TRAIN_LOGDIR="${FORMA_DATASET_DIR}/${EXP_FOLDER}/train"
@@ -141,15 +126,28 @@ mkdir -p "${EVAL_LOGDIR}"
 mkdir -p "${VIS_LOGDIR}"
 mkdir -p "${EXPORT_DIR}"
 
-# # Copy locally the trained checkpoint as the initial checkpoint.
-# TF_INIT_ROOT="http://download.tensorflow.org/models"
-# TF_INIT_CKPT="deeplabv3_pascal_train_aug_2018_01_04.tar.gz"
-# cd "${INIT_FOLDER}"
-# wget -nd -c "${TF_INIT_ROOT}/${TF_INIT_CKPT}"
-# tar -xf "${TF_INIT_CKPT}"
-# cd "${CURRENT_DIR}"
+TF_INITIAL_CHECKPOINT="${DATASET_FOLDER}/forma_37k_ckpt/model.ckpt-740000"
+INITIALIZE_LAST_LAYERS=true
+LAST_LAYERS_CONTAINS_LOGITS_ONLY=true # irrelevant
 
-# Train 10 iterations.
+# TF_INITIAL_CHECKPOINT="${WORK_DIR}/datasets/pascal_voc_seg/init_models/deeplabv3_pascal_train_aug/model.ckpt"
+# INITIALIZE_LAST_LAYERS=false
+# LAST_LAYERS_CONTAINS_LOGITS_ONLY=true
+
+# Export the trained checkpoint.
+CKPT_PATH="${TRAIN_LOGDIR}/model.ckpt-${NUM_ITERATIONS}"
+EXPORT_PATH="${EXPORT_DIR}/frozen_inference_graph.pb"
+
+# ========================== SETTINGS (END) ==========================
+# TODO: make data input dir a TF flag
+# Build form data (from ~/storage/shared/datasets/json/train_json/)
+python3 ./datasets/build_forma_data.py \
+    --dataset_size=${DATASET_SIZE} \
+    --dataset_seg_encoding_type="${DATASET_SEG_ENCODING_TYPE}" \
+    --output_dir="${FORMA_DATASET_DATA}" \
+    --list_folder="${FORMA_DATASET_LIST}"
+
+# Train.
 python3 "${WORK_DIR}"/train.py \
   --logtostderr \
   --train_split="train" \
@@ -211,9 +209,6 @@ python3 "${WORK_DIR}"/vis.py \
   --colormap_type="forma"
 
 # Export the trained checkpoint.
-CKPT_PATH="${TRAIN_LOGDIR}/model.ckpt-${NUM_ITERATIONS}"
-EXPORT_PATH="${EXPORT_DIR}/frozen_inference_graph.pb"
-
 python3 "${WORK_DIR}"/export_model.py \
   --logtostderr \
   --checkpoint_path="${CKPT_PATH}" \
