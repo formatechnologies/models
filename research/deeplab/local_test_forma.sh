9cfbf0def5e978746d1223f9fd331a5a3fe7d46c
Xin Yu Tan <dennis@formatech.com>
2019-09-17 17:13:07 -0700

    update directory structure

--- research/deeplab/local_test_forma.sh
+++ research/deeplab/local_test_forma.sh
@@ -26,18 +26,16 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Set up the working environment.
-WORK_DIR=$(pwd)
-
 # Update PYTHONPATH.
 cd ..
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
-cd ${WORK_DIR}
+cd deeplab
 
 # ========================== SETTINGS (DATASET) ==========================
 # http://hellodfan.com/2018/07/06/DeepLabv3-with-own-dataset/
 
-# DATASET_NAME="forma_1k"
+# DATASET_NAME="imaterialist1k"
+# DEEPLAB_NAME="forma_1k"
 # DATASET_SIZE=1000
 # DATASET_SEG_ENCODING_TYPE='seg_name_to_label'
 # NUM_CLASSES=9
@@ -46,7 +44,8 @@ cd ${WORK_DIR}
 # SAVE_INTERVAL_SECS=120
 # SAVE_SUMMARIES_SECS=120
 
-# DATASET_NAME="forma_1k_3"
+# DATASET_NAME="imaterialist1k"
+# DEEPLAB_NAME="forma_1k_3"
 # DATASET_SIZE=1000
 # DATASET_SEG_ENCODING_TYPE='seg_name_to_label_3'
 # NUM_CLASSES=3
@@ -55,7 +54,8 @@ cd ${WORK_DIR}
 # SAVE_INTERVAL_SECS=120
 # SAVE_SUMMARIES_SECS=120
 
-# DATASET_NAME="forma_1k_7"
+# DATASET_NAME="imaterialist1k"
+# DEEPLAB_NAME="forma_1k_7"
 # DATASET_SIZE=1000
 # DATASET_SEG_ENCODING_TYPE='seg_name_to_label_7'
 # NUM_CLASSES=7
@@ -64,7 +64,8 @@ cd ${WORK_DIR}
 # SAVE_INTERVAL_SECS=120
 # SAVE_SUMMARIES_SECS=120
 
-DATASET_NAME="forma_37k"
+DATASET_NAME="imaterialist37k"
+DEEPLAB_NAME="forma_37k"
 DATASET_SIZE=-1
 DATASET_SEG_ENCODING_TYPE='seg_name_to_label'
 NUM_CLASSES=9
@@ -73,7 +74,8 @@ NUM_ITERATIONS=740000
 SAVE_INTERVAL_SECS=1200
 SAVE_SUMMARIES_SECS=600
 
-# DATASET_NAME="forma_37k_3"
+# DATASET_NAME="imaterialist37k"
+# DEEPLAB_NAME="forma_37k_3"
 # DATASET_SIZE=-1
 # DATASET_SEG_ENCODING_TYPE='seg_name_to_label_3'
 # NUM_CLASSES=3
@@ -82,7 +84,8 @@ SAVE_SUMMARIES_SECS=600
 # SAVE_INTERVAL_SECS=1200
 # SAVE_SUMMARIES_SECS=600
 
-# DATASET_NAME="forma_37k_7"
+# DATASET_NAME="imaterialist37k"
+# DEEPLAB_NAME="forma_37k_7"
 # DATASET_SIZE=-1
 # DATASET_SEG_ENCODING_TYPE='seg_name_to_label_7'
 # NUM_CLASSES=7
@@ -104,51 +107,64 @@ FINE_TUNE_BATCH_NORM=false
 
 # ========================== SETTINGS (PATHS) ==========================
 
+# Deeplab Path
+DEEPLAB_DIR="${HOME}/storage/shared/deeplab"
+INIT_MODELS_DIR="${DEEPLAB_DIR}/init_models"
+DATASETS_DIR="${DEEPLAB_DIR}/datasets"
+EXPERIMENTS_DIR="${DEEPLAB_DIR}/experiments"
+
+# Init Models Paths
+# TF_INITIAL_CHECKPOINT="${WORK_DINIT_MODELS_DIRIR}/deeplabv3_pascal_train_aug/model.ckpt"
+# INITIALIZE_LAST_LAYERS=false
+# LAST_LAYERS_CONTAINS_LOGITS_ONLY=true
+
+# TF_INITIAL_CHECKPOINT="${INIT_MODELS_DIR}/imaterialist37k/model.ckpt-740000"
+TF_INITIAL_CHECKPOINT="${INIT_MODELS_DIR}/imaterialist37k_augmented/model.ckpt-366824"
+INITIALIZE_LAST_LAYERS=true
+LAST_LAYERS_CONTAINS_LOGITS_ONLY=true # irrelevant
+
 # Dataset Paths
-DATASET_FOLDER="${HOME}/storage/shared/datasets/deeplab_experiments"
-FORMA_DATASET_DIR="${DATASET_FOLDER}/${DATASET_NAME}"
-FORMA_DATASET_DATA="${FORMA_DATASET_DIR}/tfrecord"
-FORMA_DATASET_LIST="${FORMA_DATASET_DIR}/dataset_split"
-mkdir -p "${FORMA_DATASET_DIR}"
-mkdir -p "${FORMA_DATASET_DATA}"
-mkdir -p "${FORMA_DATASET_LIST}"
+DATASET_DIR="${DATASETS_DIR}/${DATASET_NAME}"
+DATASET_TFRECORD="${DATASET_DIR}/tfrecord"
+DATASET_SPLIT="${DATASET_DIR}/dataset_split"
+mkdir -p "${DATASET_DIR}"
+mkdir -p "${DATASET_TFRECORD}"
+mkdir -p "${DATASET_SPLIT}"
 
 # Experiment Paths
-EXP_FOLDER="exp/train_on_train_set"
-INIT_FOLDER="${FORMA_DATASET_DIR}/init_models"
-TRAIN_LOGDIR="${FORMA_DATASET_DIR}/${EXP_FOLDER}/train"
-EVAL_LOGDIR="${FORMA_DATASET_DIR}/${EXP_FOLDER}/eval"
-VIS_LOGDIR="${FORMA_DATASET_DIR}/${EXP_FOLDER}/vis"
-EXPORT_DIR="${FORMA_DATASET_DIR}/${EXP_FOLDER}/export"
-mkdir -p "${INIT_FOLDER}"
+DATE=`date +"%Y-%m-%d_%H-%M-%S"`
+HOSTNAME=`hostname`
+USER=`whoami`
+EXPERIMENT_DESCRIPTION="augmented"
+
+EXPERIMENT_NAME="${HOSTNAME}_${USER}_${DATASET_NAME}_${EXPERIMENT_DESCRIPTION}"
+EXPERIMENT_FOLDER="${EXPERIMENTS_DIR}/${EXPERIMENT_NAME}"
+TRAIN_LOGDIR="${EXPERIMENT_FOLDER}/train"
+EVAL_LOGDIR="${EXPERIMENT_FOLDER}/eval"
+VIS_LOGDIR="${EXPERIMENT_FOLDER}/vis"
+EXPORT_DIR="${EXPERIMENT_FOLDER}/export"
+mkdir -p "${EXPERIMENT_FOLDER}"
 mkdir -p "${TRAIN_LOGDIR}"
 mkdir -p "${EVAL_LOGDIR}"
 mkdir -p "${VIS_LOGDIR}"
 mkdir -p "${EXPORT_DIR}"
-
-TF_INITIAL_CHECKPOINT="${DATASET_FOLDER}/forma_37k_ckpt/model.ckpt-740000"
-INITIALIZE_LAST_LAYERS=true
-LAST_LAYERS_CONTAINS_LOGITS_ONLY=true # irrelevant
-
-# TF_INITIAL_CHECKPOINT="${WORK_DIR}/datasets/pascal_voc_seg/init_models/deeplabv3_pascal_train_aug/model.ckpt"
-# INITIALIZE_LAST_LAYERS=false
-# LAST_LAYERS_CONTAINS_LOGITS_ONLY=true
 
 # Export the trained checkpoint.
 CKPT_PATH="${TRAIN_LOGDIR}/model.ckpt-${NUM_ITERATIONS}"
 EXPORT_PATH="${EXPORT_DIR}/frozen_inference_graph.pb"
 
 # ========================== SETTINGS (END) ==========================
+
 # TODO: make data input dir a TF flag
 # Build form data (from ~/storage/shared/datasets/json/train_json/)
 python3 ./datasets/build_forma_data.py \
     --dataset_size=${DATASET_SIZE} \
     --dataset_seg_encoding_type="${DATASET_SEG_ENCODING_TYPE}" \
-    --output_dir="${FORMA_DATASET_DATA}" \
-    --list_folder="${FORMA_DATASET_LIST}"
+    --output_dir="${DATASET_TFRECORD}" \
+    --list_folder="${DATASET_SPLIT}"
 
 # Train.
-python3 "${WORK_DIR}"/train.py \
+python3 ./train.py \
   --logtostderr \
   --train_split="train" \
   --model_variant="xception_65" \
@@ -164,9 +180,9 @@ python3 "${WORK_DIR}"/train.py \
   --tf_initial_checkpoint="${TF_INITIAL_CHECKPOINT}" \
   --initialize_last_layer=${INITIALIZE_LAST_LAYERS} \
   --last_layers_contain_logits_only=${LAST_LAYERS_CONTAINS_LOGITS_ONLY} \
-  --dataset="${DATASET_NAME}" \
+  --dataset="${DEEPLAB_NAME}" \
   --train_logdir="${TRAIN_LOGDIR}" \
-  --dataset_dir="${FORMA_DATASET_DATA}" \
+  --dataset_dir="${DATASET_TFRECORD}" \
   --save_interaval_secs="${SAVE_INTERVAL_SECS}" \
   --save_summary_secs = "${SAVE_SUMMARIES_SECS}" \
   --save_summaries_images=true
@@ -174,7 +190,7 @@ python3 "${WORK_DIR}"/train.py \
 # Run evaluation. This performs eval over the full val split (1449 images) and
 # will take a while.
 # Using the provided checkpoint, one should expect mIOU=82.20%.
-python3 "${WORK_DIR}"/eval.py \
+python3 ./eval.py \
   --logtostderr \
   --eval_split="trainval" \
   --model_variant="xception_65" \
@@ -184,14 +200,14 @@ python3 "${WORK_DIR}"/eval.py \
   --output_stride=16 \
   --decoder_output_stride=4 \
   --eval_crop_size="${EVAL_CROP_SIZE}" \
-  --dataset="${DATASET_NAME}" \
+  --dataset="${DEEPLAB_NAME}" \
   --checkpoint_dir="${TRAIN_LOGDIR}" \
   --eval_logdir="${EVAL_LOGDIR}" \
-  --dataset_dir="${FORMA_DATASET_DATA}" \
+  --dataset_dir="${DATASET_TFRECORD}" \
   --max_number_of_evaluations=1
 
 # Visualize the results.
-python3 "${WORK_DIR}"/vis.py \
+python3 ./vis.py \
   --logtostderr \
   --vis_split="trainval" \
   --model_variant="xception_65" \
@@ -201,15 +217,15 @@ python3 "${WORK_DIR}"/vis.py \
   --output_stride=16 \
   --decoder_output_stride=4 \
   --vis_crop_size="${EVAL_CROP_SIZE}" \
-  --dataset="${DATASET_NAME}" \
+  --dataset="${DEEPLAB_NAME}" \
   --checkpoint_dir="${TRAIN_LOGDIR}" \
   --vis_logdir="${VIS_LOGDIR}" \
-  --dataset_dir="${FORMA_DATASET_DATA}" \
+  --dataset_dir="${DATASET_TFRECORD}" \
   --max_number_of_iterations=1 \
   --colormap_type="forma"
 
 # Export the trained checkpoint.
-python3 "${WORK_DIR}"/export_model.py \
+python3 ./export_model.py \
   --logtostderr \
   --checkpoint_path="${CKPT_PATH}" \
   --export_path="${EXPORT_PATH}" \
