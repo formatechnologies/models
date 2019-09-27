2c73a8a2ec0c400f00fa71bb15d667c4877670b9
Xin Yu Tan <dennis@formatech.com>
2019-09-27 11:33:28 -0700

    update losses plotter

--- research/deeplab/losses_plotter.py
+++ research/deeplab/losses_plotter.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -20,30 +22,44 @@ def numpy_ewma_vectorized_v2(data, window):
     return out
 
 if __name__ == '__main__':
-    BATCH_SIZE = 16
-    STEPS_PER_LOG = 10
+    HOME_DIR = os.path.expanduser('~')
+    EXPERIMENTS_DIR = os.path.join(HOME_DIR, 'storage/shared/deeplab/experiments')
 
-    with open('abq-gpu-1-losses.txt') as f:
-        lines = f.readlines()
-
-    losses = np.array([float(line.strip()[16:-1]) for line in lines if line.startswith('Total')])/BATCH_SIZE
-    examples = BATCH_SIZE*STEPS_PER_LOG*np.arange(losses.shape[0])
-    losses_ewma = numpy_ewma_vectorized_v2(losses, 100)
-
-    with open('abq-gpu-2-losses.txt') as f:
-        lines = f.readlines()
-
-    losses2 = np.array([float(line.strip()[16:-1]) for line in lines if line.startswith('Total')])/BATCH_SIZE
-    examples2 = BATCH_SIZE*STEPS_PER_LOG*np.arange(losses2.shape[0])
-    losses_ewma2 = numpy_ewma_vectorized_v2(losses2, 100)
+    experiments = [
+        {
+            'name': 'abq-gpu-1_dennis_forma54k_augmented_learn_policy_step_learn_rate_0.001_output_stride_16_batch_size_16',
+            'batch_size': 16,
+            'steps_per_log': 10,
+            'color': 'red',
+        },
+        {
+            'name': 'abq-gpu-2_dennis_forma54k_augmented_learn_policy_step_learn_rate_0.007_output_stride_16_batch_size_16_init_model_pascal',
+            'batch_size': 16,
+            'steps_per_log': 10,
+            'color': 'green',
+        },
+        {
+            'name': 'forma54k_2019-09-26_base_learn_rate_0.01_train_batch_size_16_train_crop_size_513,513_abq-gpu-2_dennis',
+            'batch_size': 16,
+            'steps_per_log': 10,
+            'color': 'blue',
+        },
+    ]
 
     plt.figure()
     plt.title('DeepLab V3+ Training Losses')
     plt.xlabel('examples')
     plt.ylabel('loss')
-    plt.plot(examples, losses, color='red', alpha=0.2)
-    plt.plot(examples, losses_ewma, color='red', label='abq-gpu-1_dennis_forma54k_augmented_learn_policy_step_learn_rate_0.001_output_stride_16_batch_size_16')
-    plt.plot(examples2, losses2, color='green', alpha=0.2)
-    plt.plot(examples2, losses_ewma2, color='green', label='abq-gpu-2_dennis_forma54k_augmented_learn_policy_step_learn_rate_0.007_output_stride_16_batch_size_16_init_model_pascal')
+    for experiment in experiments:
+
+        with open(os.path.join(EXPERIMENTS_DIR, experiment['name'], 'log.txt')) as f:
+            lines = f.readlines()
+
+        losses = np.array([float(line.strip()[16:-1]) for line in lines if line.startswith('Total')])/experiment['batch_size']
+        examples = experiment['batch_size']*experiment['steps_per_log']*np.arange(losses.shape[0])
+        losses_ewma = numpy_ewma_vectorized_v2(losses, 100)
+
+        plt.plot(examples, losses, color=experiment['color'], alpha=0.2)
+        plt.plot(examples, losses_ewma, color=experiment['color'], label=experiment['name'])
     plt.legend()
     plt.show()
